f3cf7f84b94fec772521b94a2253b8b70ccca6e9
Kevin Netherton <kevin.netherton@gov.bc.ca>
2021-03-11 19:09:51 -0800

    adding code to get python3

---
+++ cicd/getPython.bat
@@ -0,0 +1,28 @@
+SET curDir=%cd%
+SET pyinstallDir=%curDir%\python
+SET pythoninstaller=python39.zip
+SET pythoninstallerFullPath=%pyinstallDir%\%pythoninstaller%
+SET pythonexeFullPath=%pyinstallDir%/python.exe
+SET PATH=%cygwinhome%/bin;%PATH%
+
+:: needs cygwinhome
+
+if NOT EXIST %pyinstallDir% (
+    mkdir %pyinstallDir%
+)
+if NOT EXIST %pythoninstallerFullPath% (
+    curl https://www.python.org/ftp/python/3.9.2/python-3.9.2-embed-amd64.zip -o %pythoninstallerFullPath%
+)
+if NOT EXIST %pythoninstaller% (
+    cd %pyinstallDir%
+    %cygwinhome%/bin/bash.exe -c "unzip $pythoninstaller"
+)
+if NOT EXIST %pythonexeFullPath% (
+    cd %pyinstallDir%
+    %cygwinhome%/bin/bash.exe -c "unzip $pythoninstaller"
+    %cygwinhome%/bin/bash.exe -c "echo 'import site' >> python39._pth"
+    
+)
+
+
+
