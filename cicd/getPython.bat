d9d41c32a5750ec03e1fd4183899dcef71e3c4bb
Kevin Netherton <kevin.netherton@gov.bc.ca>
2021-03-11 19:10:03 -0800

    typos in getpython

--- cicd/getPython.bat
+++ cicd/getPython.bat
@@ -21,7 +21,8 @@ if NOT EXIST %pythonexeFullPath% (
     cd %pyinstallDir%
     %cygwinhome%/bin/bash.exe -c "unzip $pythoninstaller"
     %cygwinhome%/bin/bash.exe -c "echo 'import site' >> python39._pth"
-    
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    pythonexeFullPath get-pip.py
 )
 
 
