64f3d706eb989ad671bf6f9b61cec7fe7371527f
KYSIEMENS <kyle.siemens@gov.bc.ca>
2024-08-21 16:11:48 -0700

    Modified for multiprocessing data extraction

--- src/extract_forecast_main.py
+++ src/extract_forecast_main.py
@@ -1,11 +1,20 @@
 from datetime import datetime as dt, timedelta
 from downloads import download_models
 from processing import extract_model_data
+import time
 
+
+#baseline: 262.8 s
+#With small_grib: 41.7 s
 if __name__ == '__main__':
+    start = time.time()
     current_date = dt.now()
     latest_run = current_date.replace(hour=12)
     download_models.main('reps', latest_run)
     #download_models.main('geps', latest_run)
-    #extract_model_data.main(latest_run, 'reps')
+
+    extract_model_data.main(latest_run, 'reps')
     #extract_model_data.main(latest_run, 'geps')
+    end = time.time()
+    time_taken = (end - start)
+    print(f'total run time = {time_taken} s')
